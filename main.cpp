8986de81e69ca1c94fdba26dda0e9c4d60736a84
ei0 <48590828+ei0@users.noreply.github.com>
2020-04-14 08:30:40 +0800

    Create main.cpp
    支持大数版本的测试

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+#include "RSA.hpp"
+using std::cin;
+using std::cout;
+using std::endl;
+int main() {
+	//RSA rsa;
+	//key ky = rsa.getallkey();
+	//cout << "(e,n)" << endl;
+	//cout << ky.m_ekey << ' ' << ky.m_pkey << endl;
+	//cout << "(d,n)" << endl;
+	//cout << ky.m_dkey << ' ' << ky.m_pkey << endl;
+	//rsa.encipher("1.txt", "1d.txt");
+	//rsa.decrypt("1d.txt", "1ds.txt");
+	//cout << "完成！" << endl;
+	//std::string src, dst, dst_src;
+	//while (cin >> src >> dst >> dst_src) {
+	//	rsa.encipher(src.c_str(), dst.c_str());
+	//	rsa.decrypt(dst.c_str(), dst_src.c_str());
+	//	cout << "完成！" << endl;
+	//}
+	cout << "OK!" << endl;
+	return 0;
+}
